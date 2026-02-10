52c52b5c71977b0edd2117629f7dbefaa7178cf1
Lucas Daniel <lucas.daniel@aluno.ufca.edu.br>
2026-02-10 18:55:42 -0300

    inicio de encapsulamento

--- domain/aluno.py
+++ domain/aluno.py
@@ -11,11 +11,32 @@ class Aluno(Usuario):
 
     @property
     def matricula(self):
-        return self._matricula
+        return self.__matricula
+    
+    @matricula.setter
+    def matricula(self, nova_matricula):
+        if isinstance(nova_matricula, str):
+            self.__matricula = nova_matricula
+
+        else:
+            raise TypeError("Matricula deve ser String!")
+        
 
     @property
     def curso(self):
-        return self._curso
+        return self.__curso
+
+    @curso.setter
+    def curso(self, novo_curso):
+        if not isinstance(novo_curso, str):
+            raise TypeError("Curso deve ser String!")
+        
+        if not isinstance(novo_curso, Curso):
+            raise TypeError("Esperava instância de curso.")
+        
+        else:
+            self.__curso = novo_curso
+        
 
     @property
     def historico(self):
